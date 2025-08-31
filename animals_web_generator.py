80bf1926220e1dec98cc94453e43691e784c7057
waadhmd <waad.hamoud94@gmail.com>
2025-08-31 15:18:30 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,27 +12,29 @@ def load_html_template(file_path):
     with open(file_path, 'r',encoding="utf-8") as template_file:
         return template_file.read()
 
-def format_animals_text():
-    animals = load_data(ANIMALS_DATA_FILENAME)
+def format_animals_text(animals):
     output = ''
     for animal in animals:
+        output += '<li class = "cards__item">'
         if 'name' in animal:
-            output += f"Name: {animal['name']}\n"
+            output += f"Name: {animal['name']}<br/>\n"
 
         diet = animal.get('characteristics',{}).get('diet')
         if diet:
-            output += f"Diet: {diet}\n"
+            output += f"Diet: {diet}<br/>\n"
 
         if 'locations' in animal:
-            output += f"Location: {animal['locations'][0]}\n"
+            output += f"Location: {animal['locations'][0]}<br/>\n"
 
         animal_type_value = animal.get('characteristics',{}).get('type')
         if animal_type_value:
-            output += f"Type: {animal_type_value}\n"
+            output += f"Type: {animal_type_value}<br/>\n"
+        output += '</li>'
     return output
 
 def main():
-    animal_info_text = format_animals_text()
+    animals = load_data(ANIMALS_DATA_FILENAME)
+    animal_info_text = format_animals_text(animals)
     html_template = load_html_template(TEMPLATE_FILENAME)
     rendered_html = html_template.replace('__REPLACE_ANIMALS_INFO__',animal_info_text)
 
