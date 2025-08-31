0cfd23ef964a85feeb267818801a5ba8df3b5522
waadhmd <waad.hamoud94@gmail.com>
2025-08-31 14:58:14 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,27 +1,43 @@
 import json
 
 ANIMALS_DATA_FILENAME = 'animals_data.json'
+TEMPLATE_FILENAME = 'animals_template.html'
+OUTPUT_HTML_FILENAME = "animals.html"
 
 def load_data(file_path):
     with open(file_path , 'r') as handle:
         return json.load(handle)
 
-def print_data():
+def load_html_template(file_path):
+    with open(file_path, 'r',encoding="utf-8") as template_file:
+        return template_file.read()
+
+def format_animals_text():
     animals = load_data(ANIMALS_DATA_FILENAME)
+    output = ''
     for animal in animals:
-        print()
         if 'name' in animal:
-            print(f"Name: {animal['name']}")
+            output += f"Name: {animal['name']}\n"
 
         diet = animal.get('characteristics',{}).get('diet')
         if diet:
-            print(f"Diet: {diet}")
+            output += f"Diet: {diet}\n"
 
         if 'locations' in animal:
-            print(f"Location: {animal['locations'][0]}")
+            output += f"Location: {animal['locations'][0]}\n"
 
         animal_type_value = animal.get('characteristics',{}).get('type')
         if animal_type_value:
-            print(f"Type: {animal_type_value}")
-print_data()
+            output += f"Type: {animal_type_value}\n"
+    return output
 
+def main():
+    animal_info_text = format_animals_text()
+    html_template = load_html_template(TEMPLATE_FILENAME)
+    rendered_html = html_template.replace('__REPLACE_ANIMALS_INFO__',animal_info_text)
+
+    with open(OUTPUT_HTML_FILENAME , 'w') as output_file:
+        output_file.write(rendered_html)
+
+if __name__ == '__main__':
+    main()
