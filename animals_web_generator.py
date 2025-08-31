67435badde1446aead686069fb715258efdc8387
waadhmd <waad.hamoud94@gmail.com>
2025-08-31 15:46:40 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,29 +12,38 @@ def load_html_template(file_path):
     with open(file_path, 'r',encoding="utf-8") as template_file:
         return template_file.read()
 
-def format_animals_text(animals):
+def serialize_animal(animal:dict):
+    output = ""
+
+    output += '<li class="cards__item">'
+
+    if 'name' in animal:
+        output += f"<div class='card__title' > {animal['name']} </div>"
+    output += '<p class="card__text">'
+    diet = animal.get('characteristics', {}).get('diet')
+
+    if diet:
+        output += f"<strong>Diet:</strong>{diet} <br/>"
+
+    if 'locations' in animal:
+        output += f"<strong>Location:</strong>{animal['locations'][0]} <br/>"
+    animal_type_value = animal.get('characteristics', {}).get('type')
+
+    if animal_type_value:
+        output += f"<strong>Type:</strong>{animal_type_value} <br/>"
+    output += '</p>'
+    output += '</li>'
+    return output
+
+def render_animals_text(animals:list):
     output = ''
     for animal in animals:
-        output += '<li class = "cards__item">'
-        if 'name' in animal:
-            output += f"Name: {animal['name']}<br/>\n"
-
-        diet = animal.get('characteristics',{}).get('diet')
-        if diet:
-            output += f"Diet: {diet}<br/>\n"
-
-        if 'locations' in animal:
-            output += f"Location: {animal['locations'][0]}<br/>\n"
-
-        animal_type_value = animal.get('characteristics',{}).get('type')
-        if animal_type_value:
-            output += f"Type: {animal_type_value}<br/>\n"
-        output += '</li>'
+        output += serialize_animal(animal)
     return output
 
 def main():
     animals = load_data(ANIMALS_DATA_FILENAME)
-    animal_info_text = format_animals_text(animals)
+    animal_info_text = render_animals_text(animals)
     html_template = load_html_template(TEMPLATE_FILENAME)
     rendered_html = html_template.replace('__REPLACE_ANIMALS_INFO__',animal_info_text)
 
