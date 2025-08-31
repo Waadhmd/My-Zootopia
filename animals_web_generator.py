4a9a35f07badbeba3777aab997b3a4e396a9d23f
waadhmd <waad.hamoud94@gmail.com>
2025-08-31 16:22:36 +0200

    Improve HTML structure for animal cards

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -5,43 +5,66 @@ TEMPLATE_FILENAME = 'animals_template.html'
 OUTPUT_HTML_FILENAME = "animals.html"
 
 def load_data(file_path):
+    """
+     Load JSON data from a file.
+    Returns:
+        list: Parsed JSON data as a list of dictionaries
+    """
     with open(file_path , 'r') as handle:
         return json.load(handle)
 
 def load_html_template(file_path):
+    """
+     Load an HTML template file.
+    """
     with open(file_path, 'r',encoding="utf-8") as template_file:
         return template_file.read()
 
 def serialize_animal(animal:dict):
+    """
+    Convert a single animal dictionary into an HTML <li> element
+    styled as a card with its details.
+    """
     output = ""
 
     output += '<li class="cards__item">'
 
     if 'name' in animal:
         output += f"<div class='card__title' > {animal['name']} </div>"
-    output += '<p class="card__text">'
+    output += '<div class="card__text">'
+    output += '<ul class="cards">'
     diet = animal.get('characteristics', {}).get('diet')
 
     if diet:
-        output += f"<strong>Diet:</strong>{diet} <br/>"
+        output += f"<li class='card__detail'><strong>Diet:</strong> {diet} </li>"
 
     if 'locations' in animal:
-        output += f"<strong>Location:</strong>{animal['locations'][0]} <br/>"
+        output += f"<li class='card__detail'><strong>Location:</strong> {animal['locations'][0]} </li>"
     animal_type_value = animal.get('characteristics', {}).get('type')
 
     if animal_type_value:
-        output += f"<strong>Type:</strong>{animal_type_value} <br/>"
-    output += '</p>'
+        output += f"<li class='card__detail'><strong>Type:</strong> {animal_type_value} </li>"
+    output += '</ul>'
+    output += '</div>'
     output += '</li>'
     return output
 
 def render_animals_text(animals:list):
+    """
+    Convert a list of animal dictionaries into a single HTML string
+    """
     output = ''
     for animal in animals:
         output += serialize_animal(animal)
     return output
 
 def main():
+    """
+    - Loads animal data from JSON
+    - Converts data into HTML card list
+    - Inserts card list into the HTML template
+    - Saves the rendered HTML file
+    """
     animals = load_data(ANIMALS_DATA_FILENAME)
     animal_info_text = render_animals_text(animals)
     html_template = load_html_template(TEMPLATE_FILENAME)
